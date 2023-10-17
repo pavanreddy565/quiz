281de6fd599e5fc1c15310ee48478509d9b8b404
pavanreddy54 <sasipavan94@gmail.com>
2023-10-17 16:35:28 +0530

    css border change properties added

--- quiz.js
+++ quiz.js
@@ -4,11 +4,13 @@ var answer = { 1: 'Cascading Style Sheets', 2: 'Hyper Text Markup Language', 3: 
 
 function que1() {
     var con = 'question' + q;
+    var r=document.querySelector(':root')
     ques = 'q' + q;
     var ans = document.quiz.elements['question' + q].value;
     if (ans == answer[q]) {
         total++;
-        document.getElementById(ques).style.border = "2px solid green";
+        r.style.setProperty('--clr-1','green');
+        r.style.setProperty('--clr-2','red');
     }
     
     document.getElementById('q' + q).style.display = "none";
