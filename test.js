fb9857e243af94f70cad70fe13d59b59c00db8f8
pavanreddy565 <pavanreddy5658@gmail.com>
2024-01-12 12:47:29 +0530

    quiz application has been created

---
+++ test.js
@@ -0,0 +1,17 @@
+var q=1
+ans='Cascading Style Sheets'
+var answer={1:'Cascading Style Sheets',2:'Hyper Text Markup Language',3:'JavaScript'}
+function que1(){
+    var total=0
+    var con='question'+q
+    console.log(con)
+    //var ans=document.quiz.con.value;
+    if (ans==answer[q]){
+        total++;
+    }
+    //document.getElementById('q'+q).style.display=none;
+    q++;
+    //document.getElementById('q'+q).style.display=block;
+
+    
+}
